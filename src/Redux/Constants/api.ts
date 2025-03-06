3cdae68a5157e9522ba1b2a467e03bae23b993b0
Alex <casanovaalex61@gmail.com>
2025-03-06 14:12:53 -0600

    primer commit

---
+++ src/Redux/Constants/api.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:9000", 
+  // baseURL: "https://mex-shoes.vercel.app", 
+
+});
+
+export default api;
---
+++ src/pages/categorias/categorias.tsx
@@ -0,0 +1,15 @@
+ 
+// export default DashboardPage;
+import { FC, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+ 
+ 
+const CategoriasPage: FC = function () {
+  
+  return (
+    <h1>hola mundo</h1>
+  );
+};
+
+export default CategoriasPage;
+
